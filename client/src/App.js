a13938a2860328954184ffe589d6e698141a823a
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2024-02-03 16:04:20 +0530

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,20 @@ import "./App.css";
 import SideBar from "./components/sideBar/Sidebar";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Products from "./pages/Products";
 import Category from "./pages/Category";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function App() {
+  let navigate = useNavigate();
   let currentTab = useSelector((state) => state.sideBar.currentTab);
-
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (!token) {
+      return navigate("/login");
+    }
+  }, [navigate]);
   return (
     <>
       <Navbar></Navbar>
